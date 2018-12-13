c60805f77b3b87b8ec2df06d93d68ad637f86ef7
misterbastean <misterbastean@gmail.com>
2018-12-13 14:12:23 -0500

    Still fixing

--- app.js
+++ app.js
@@ -31,6 +31,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 // Setup view engine
+app.set('views', path.join(__dirname + '/views'))
 app.set('view engine', 'ejs');
 
 // Authentication
@@ -58,7 +59,6 @@ app.use((req, res, next) => {
 
 // Serve public folder
 app.use('/public', express.static(path.join(__dirname + '/public')));
-app.set('views', __dirname + '/views');
 
 // Use routes
 app.use('/tesla', teslaRoutes);
