be9e9644eebadd518b1981b76b533ccc91de9602
misterbastean <misterbastean@gmail.com>
2018-12-13 14:15:17 -0500

    Still working on it

--- app.js
+++ app.js
@@ -31,7 +31,6 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 // Setup view engine
-app.set('views', path.join(__dirname + '/views'))
 app.set('view engine', 'ejs');
 
 // Authentication
@@ -57,8 +56,9 @@ app.use((req, res, next) => {
   next();
 });
 
-// Serve public folder
+// Serve public folder and views folder
 app.use('/public', express.static(path.join(__dirname + '/public')));
+app.set('views', path.join(__dirname, 'views'));
 
 // Use routes
 app.use('/tesla', teslaRoutes);
